bdfa8f31159f64e19ecff58272a4263c603d8317
ashburndev <ashburndev@gmail.com>
2020-10-12 08:46:50 -0400

    first commit

---
+++ src/main/java/com/ashburndev/App.java
@@ -0,0 +1,17 @@
+package com.ashburndev;
+
+/**
+ * Hello world!
+ */
+public class App {
+    /*
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    */
+
+    public static void main(String[] args) {
+        System.out.println("Hello Lucene");
+    }
+}
